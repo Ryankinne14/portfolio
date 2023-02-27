fd93bcd960b3bce0b87639fa66175e4bc2abb38c
Ryan Kinne <ryanskinne18@gmail.com>
2023-02-27 06:11:23 -0500

    First commit for portfolio

---
+++ javasc.js
@@ -0,0 +1,10 @@
+
+var light = document.getElementById("light");
+var lightTwo = document.getElementById("light-Two");
+    function toggleBtn(){
+        light.classList.toggle("on");
+        
+    }
+    function toggleBtn2(){
+        lightTwo.classList.toggle("onTwo");
+    }
